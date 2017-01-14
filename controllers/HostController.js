a2214c76a1429108a76c99e24d49a7d8258fcf35
Jean-PhilippeD <jp.danree@free.fr>
2017-01-14 18:21:16 +0100

    fix wrong resolve

--- controllers/HostController.js
+++ controllers/HostController.js
@@ -1,11 +1,6 @@
 /** 
-  * Gladys Project
-  * http://gladysproject.com
-  * Software under licence Creative Commons 3.0 France 
-  * http://creativecommons.org/licenses/by-nc-sa/3.0/fr/
-  * You may not use this software for commercial purposes.
-  * @author :: Jean-Philippe Danrée
-  */
+* @author :: Jean-Philippe Danrée
+*/
 
   
 module.exports = {
